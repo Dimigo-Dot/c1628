14f1e7e5cfe9db86bcd644c8b41103267b5156f8
최재민 <admin@chicken-moo.com>
2022-08-17 16:00:54 +0900

    :sparkles: 입력을 배워보았어요 ^^

--- c1628/main.cpp
+++ c1628/main.cpp
@@ -1,8 +1,16 @@
 #include <stdio.h>
 int main() {
-    int myGrade = 1;
-    char myMajor[] = "HD";
-    char myName[] = "최재민";
+    int myGrade;
+    char myMajor[3], myName[10];
+    
+    printf("학년 입력! ");
+    scanf("%d", &myGrade);
+    
+    printf("전공 입력해 주라: ");
+    scanf("%s", myMajor);
+    
+    printf("이름을 입력해보는게 좋을거야 ^^: ");
+    scanf("%s", myName);
     
     
     printf("%d학년 %s과 %s :)\n", myGrade, myMajor, myName);
