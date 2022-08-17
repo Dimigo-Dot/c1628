ea314fd1155aea80d6b331652dbd6872ef98ee04
최재민 <admin@chicken-moo.com>
2022-08-17 15:23:57 +0900

    :tada: Dot. 동아리 프로젝트 시작

---
+++ c1628/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  c1628
+//
+//  Created by 최재민 on 2022/08/17.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
